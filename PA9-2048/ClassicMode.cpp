7944f974542b8ea1bca0bbebf1740dc4abfac829
Kyla-Jones <kyla.jones1@wsu.edu>
2024-12-02 15:18:24 -0800

    testing
    attempting to fix my github

--- PA9-2048/ClassicMode.cpp
+++ PA9-2048/ClassicMode.cpp
@@ -7,3 +7,6 @@ void ClassicMode::calculateScore(int tileValue) //, sf::RenderWindow& window)
 	score += tileValue; // adds merged tile value to score
 	updateScoreText(); // update score display
 }
+
+
+// testing
